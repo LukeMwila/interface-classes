db1c0c5cc9af6043b0b471278f6bb0d72c847ca1
Luke Mwila <outlier.developer@gmail.com>
2018-02-08 01:15:48 +0200

    Initial push

---
+++ src/com/company/Canine.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Canine extends Animal {
+    private String typeTeeth;
+
+    public String getTypeTeeth() {
+        return typeTeeth;
+    }
+
+    public void setTypeTeeth(String typeTeeth) {
+        this.typeTeeth = typeTeeth;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Dog bolt = new Dog();
+        bolt.setName("Bolt");
+
+        System.out.println(bolt.getName());
+    }
+}
